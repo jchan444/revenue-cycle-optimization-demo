d2f10cf542448d81dccf13b33e8cf7e96f2f35db
jchan316 <jchan316@gatech.edu>
2026-04-04 23:37:04 -0700

    added env variable for vercel

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -17,7 +17,7 @@ import {
 } from "../types/claim";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: process.env.REACT_APP_API_URL as string || "http://localhost:8000",
   timeout: 5000,
 });
 
